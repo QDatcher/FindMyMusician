ecc78f540b49fdf380e9167211d554db5197d0d0
jrocksymphonia <122930110+jrocksymphonia@users.noreply.github.com>
2023-05-15 19:46:02 -0400

    added user model

---
+++ models/User.js
@@ -0,0 +1,68 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        first_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        last_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+          },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+              isEmail: true,
+            },
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+              len: [8],
+            },
+        },
+        bio: {
+            type: DataTypes.STRING,
+        },
+        project_links: {
+            type: DataTypes.STRING,
+        },
+        location: {
+            type: DataTypes.STRING,
+        },
+        profile_pic: {
+            type: DataTypes.STRING,
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'User',
+    }
+);
+
+module.exports = User;
