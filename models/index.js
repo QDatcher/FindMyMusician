7422acf10941bedb1a40bf3d18ef5a87260a839d
Quise <darkquise@gmail.com>
2023-05-16 12:31:23 -0400

    added to the index file in the models folder to include comments

---
+++ models/index.js
@@ -0,0 +1,26 @@
+const User = require('./User');
+const Posts = require('./Posts');
+const Comments = require('./Comments')
+
+User.hasMany(Posts, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Posts.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+
+Posts.hasMany(Comments, {
+  foreignKey: 'post_id',
+  onDelete: 'CASCADE'
+});
+
+Comments.belongsTo(Posts, {
+  foreignKey: 'post_id'
+})
+
+
+
+module.exports = { User, Posts };
