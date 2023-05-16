86ff85431548be1c05d3d6e2382c8a15d1f9c081
jrocksymphonia <122930110+jrocksymphonia@users.noreply.github.com>
2023-05-15 21:07:07 -0400

    added posts model and a hook to the user model

---
+++ models/Posts.js
@@ -0,0 +1,48 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Posts extends Model {}
+
+Posts.init(
+    {
+        id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        title: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        text_content: {
+          type: DataTypes.STRING,
+        },
+        date_created: {
+          type: DataTypes.DATE,
+          allowNull: false,
+          defaultValue: DataTypes.NOW,
+        },
+        tag: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        user_id: {
+          type: DataTypes.INTEGER,
+          references: {
+            model: 'user',
+            key: 'id',
+          },
+        },
+      },
+      {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'posts',
+      }
+
+);
+
+module.exports = Posts;
