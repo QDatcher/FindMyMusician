a02a473c3084f59105eef060df95671256a0201e
jrocksymphonia <122930110+jrocksymphonia@users.noreply.github.com>
2023-05-16 13:34:36 -0400

    fixed some spacing, added comments model

---
+++ models/Comments.js
@@ -0,0 +1,48 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comments extends Model {}
+
+Comments.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      text_content: {
+        type: DataTypes.STRING,
+      },
+      date_created: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+      },
+      tag: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'user',
+          key: 'id',
+        },
+      },
+      post_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'posts',
+          key: 'id',
+        },
+      }
+    },
+    {
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'posts',
+    }
+);
