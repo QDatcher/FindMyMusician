5e94a7a6cfd3923c7355590667798a2134a08c74
ashleytrollinger <ashley@trollingers.com>
2023-05-16 20:05:27 -0400

    got the two buttons from the html and then created the functions to be called onclick

---
+++ public/scripts/index.js
@@ -0,0 +1,10 @@
+var logIn = document.getElementById('logIn');
+var signUp = document.getElementById('signUp');
+
+function logIn() {
+    window.location.href = "signin.html";
+}
+
+function signUp() {
+    window.location.href = "signUp.html";
+}
