2e4510531024778025cf2fbb67fec918290f0015
Quise <darkquise@gmail.com>
2023-05-17 16:56:44 -0400

    updated login and signup js files

---
+++ public/scripts/signUp.js
@@ -0,0 +1,24 @@
+const signUpFormHandler = async (event) => {
+    event.preventDefault();
+
+    const firstName = document.getElementById('firstName').value.trim();
+    const lastName = document.getElementById('lastName').value.trim();
+    const username = document.getElementById('username').value.trim();
+    const password = document.getElementById('password').value.trim();
+    const bio = document.getElementById('bio').value.trim();
+    const link = document.getElementById('link').value.trim();
+
+    if (firstName && lastName && username && email && password) {
+        const response = await fetch('/api/users', {
+          method: 'POST',
+          body: JSON.stringify({ firstName, lastName, username, email, password, bio, link }),
+          headers: { 'Content-Type': 'application/json' },
+        });
+    
+        if (response.ok) {
+          document.location.replace('/profile');
+        } else {
+          alert(response.statusText);
+        }
+      }
+};
