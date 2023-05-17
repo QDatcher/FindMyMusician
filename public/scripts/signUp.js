60eabe8c9a6ab6947aa577606cb9c306905a3761
Quise <darkquise@gmail.com>
2023-05-17 17:08:23 -0400

    added the email to email places

--- public/scripts/signUp.js
+++ public/scripts/signUp.js
@@ -4,6 +4,7 @@ const signUpFormHandler = async (event) => {
     const firstName = document.getElementById('firstName').value.trim();
     const lastName = document.getElementById('lastName').value.trim();
     const username = document.getElementById('username').value.trim();
+    const email = document.getElementById('email').value.trim();
     const password = document.getElementById('password').value.trim();
     const bio = document.getElementById('bio').value.trim();
     const link = document.getElementById('link').value.trim();
