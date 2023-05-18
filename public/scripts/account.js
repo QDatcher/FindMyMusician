c7d6f936692d5e73c90ec4ab8d5de234142f855b
ashleytrollinger <ashley@trollingers.com>
2023-05-18 16:54:19 -0400

    Undoing what I have done because I am a silly goose and doing the wrong thing

--- public/scripts/account.js
+++ public/scripts/account.js
@@ -3,14 +3,3 @@ var username = document.getElementById("username");
 var accountInfo = document.getElementById("accountInfo");
 
 //TODO: get the database information and query the correct account information (username) (location,bio,links) then append
-//Referencing most recent student mini project 
-
-const accountHandler = async (event) => {
-    event.preventDefault();
-
-    const username = document.querySelector('#username').value.trim();
-    const first_Name = document.querySelector('#first_name').value.trim();
-    const last_name = document.querySelector('#last_name').value.trim();
-    const bio = document.querySelector('#bio').value.trim()
-
-}
