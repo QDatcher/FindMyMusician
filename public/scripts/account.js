868aa65326007b84cdd5dcb3a156a992515a4f60
ashleytrollinger <ashley@trollingers.com>
2023-05-18 17:24:49 -0400

    homePageHandler created

--- public/scripts/account.js
+++ public/scripts/account.js
@@ -2,4 +2,28 @@
 var username = document.getElementById("username");
 var accountInfo = document.getElementById("accountInfo");
 
-//TODO: get the database information and query the correct account information (username) (location,bio,links) then append
+//Event Handler to get to homepage from the account page
+const homePageHandler = async (event) => {
+    event.preventDefault();
+
+    const title = document.querySelector('#title').value.trim();
+    const text_content = document.querySelector('#text_content').value.trim();
+    const tag = document.querySelector('#tag').value.trim();
+
+    if(title && text_content && tag){
+        //Send a GET request to the API for post data
+        const response = fetch ('/models/Posts', {
+            method: "POST",
+            body: JSON.stringify({title, text_content, tag}),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        
+        if(response.ok){
+            document.location.replace('/index');
+        }else{
+            alert((await response).statusText);
+        }
+    }
+} 
