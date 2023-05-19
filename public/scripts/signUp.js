54569b9deb61ef1d843cee1178f4db9e639f6e2c
ashleytrollinger <ashley@trollingers.com>
2023-05-18 20:09:13 -0400

    Added functionality to the buttons on the sign up page

--- public/scripts/signUp.js
+++ public/scripts/signUp.js
@@ -1,25 +1,39 @@
 const signUpFormHandler = async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const firstName = document.getElementById('firstName').value.trim();
-    const lastName = document.getElementById('lastName').value.trim();
-    const username = document.getElementById('username').value.trim();
-    const email = document.getElementById('email').value.trim();
-    const password = document.getElementById('password').value.trim();
-    const bio = document.getElementById('bio').value.trim();
-    const link = document.getElementById('link').value.trim();
+  const firstName = document.getElementById('firstName').value.trim();
+  const lastName = document.getElementById('lastName').value.trim();
+  const username = document.getElementById('username').value.trim();
+  const email = document.getElementById('email').value.trim();
+  const password = document.getElementById('password').value.trim();
+  const bio = document.getElementById('bio').value.trim();
+  const link = document.getElementById('link').value.trim();
 
-    if (firstName && lastName && username && email && password) {
-        const response = await fetch('/api/users', {
-          method: 'POST',
-          body: JSON.stringify({ firstName, lastName, username, email, password, bio, link }),
-          headers: { 'Content-Type': 'application/json' },
-        });
-    
-        if (response.ok) {
-          document.location.replace('/profile');
-        } else {
-          alert(response.statusText);
-        }
-      }
+  if (firstName && lastName && username && email && password) {
+    const response = await fetch('/api/users', {
+      method: 'POST',
+      body: JSON.stringify({ firstName, lastName, username, email, password, bio, link }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert(response.statusText);
+    }
+  }
 };
+var logIn = document.getElementById("logIn");
+var signUp = document.getElementById("signUp");
+
+function logInFunction() {
+  window.location.href = "/views/";
+};
+
+logIn.addEventListener("click", logInFunction);
+
+function signUpFunction() {
+  window.location.href = "/views/signUp.handlebars";
+};
+
+signUp.addEventListener("click", signUpFunction);
