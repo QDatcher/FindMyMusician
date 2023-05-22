0b94e3d4154cd961bceed42a2938e86c3a126bb9
Quise <darkquise@gmail.com>
2023-05-22 12:05:41 -0400

    added some more functionality code to the make post section

--- public/scripts/post.js
+++ public/scripts/post.js
@@ -25,3 +25,30 @@ const postFormHandler = async (event) => {
 const postForm = document.getElementById('postForm');
 
 postForm.addEventListener('submit', postFormHandler)
+
+
+
+const postFeed = document.getElementById('postFeed')
+const hidePostButton = document.getElementById('hide-post-maker')
+const makePostButton = document.getElementById('make-post')
+
+hidePostButton.style.display = 'none';
+postForm.style.display = 'none';
+
+
+const hidePostMaker = (event) => {
+  postForm.style.display = 'none';
+  makePostButton.style.display = 'block';
+  hidePostButton.style.display = 'none';
+  postFeed.style.display = 'block'
+}
+
+const showPostMaker = (event) => {
+  postForm.style.display = 'block';
+  makePostButton.style.display = 'none';
+  hidePostButton.style.display = 'block';
+  postFeed.style.display = 'none'
+}
+
+hidePostButton.addEventListener('click', hidePostMaker)
+makePostButton.addEventListener('click', showPostMaker)
