b496f45bbbe7b66b1129d3b9d77ebd4227cd435e
jrocksymphonia <122930110+jrocksymphonia@users.noreply.github.com>
2023-05-21 21:12:53 -0400

    forget the package.json, added logout.js in public folder, fixed my connection in the config folder

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 let sequelize;
-
+console.log(process.env)
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
@@ -11,7 +11,7 @@ if (process.env.JAWSDB_URL) {
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: 'localhost',
+      host: '127.0.0.1',
       dialect: 'mysql',
       port: 3306
     }
