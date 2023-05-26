be81b85cb95fa1b89f4d37ab9fa3cb688b145c8e
Quise <darkquise@gmail.com>
2023-05-26 08:55:59 -0400

    final touch hopefully

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
-
+const { clog } = require('./middleware/clog')
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
@@ -30,7 +30,7 @@ const sess = {
 };
 
 app.use(session(sess));
-
+app.use(clog)
 // Inform Express.js on which template engine to use
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
